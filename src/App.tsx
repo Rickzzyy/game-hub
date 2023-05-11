c351bae8cbf9f11b4b71926f664f218382c11c1d
Rickzzyy <ricards.taukuls@inbox.lv>
2023-05-11 17:27:16 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
-	return <Button colorScheme='blue'>Button</Button>
+	return <Button colorScheme='blue'>Buttonss</Button>
 }
 
 export default App
